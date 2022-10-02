aead6ce139c9732237033e8d6151a3137d4814ac
Mahbubur Rahman Turzo <turzo@newgen-bd.com>
2022-10-02 17:52:34 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import MyImage from "./assets/screenshot.png";
 
 function App() {
   return (
     <div className="App">
+      <img src={MyImage} alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This site is under construction
         </a>
       </header>
     </div>
